7a5c605bd3343324c1af28c1ed11bd55f603aa47
hywu1996 <35641322+hywu1996@users.noreply.github.com>
2018-07-31 23:07:40 -0400

    images

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-app.use(express.static('public'));
+app.use(express.static(__dirname +'./../client/dist'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 const port = process.env.PORT || 5000;
